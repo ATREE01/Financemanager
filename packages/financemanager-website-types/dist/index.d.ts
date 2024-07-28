366f7715085b69d12b3cb923ea607cb4f3804910
atree <anyu9898@yahoo.com>
2024-07-28 15:12:23 +0800

    feat(type): add financemanager type package

---
+++ packages/financemanager-website-types/dist/index.d.ts
@@ -0,0 +1,21 @@
+interface Register {
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface User {
+  id: string;
+  profile: {
+    email: string;
+    username: string;
+  };
+}
+
+interface CreateUser {
+  username: string;
+  email: string;
+  hashedPassword: string;
+}
+
+export type { CreateUser, Register, User };
---
+++ packages/financemanager-website-types/src/user/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: string;
+  profile: {
+    is: string;
+    email: string;
+    username: string;
+  };
+}
