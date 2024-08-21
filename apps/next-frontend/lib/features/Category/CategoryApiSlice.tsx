701dfb9fbef5555feb86e4fa1d26aa00196a132d
atree <anyu9898@yahoo.com>
2024-08-21 15:56:47 +0800

    feat(frontend): create Category Slice/ApiSlice

---
+++ apps/next-frontend/lib/features/Category/CategoryApiSlice.tsx
@@ -0,0 +1,50 @@
+import { Category } from "@financemanager/financemanager-webiste-types";
+
+import { apiSlice } from "@/lib/api/apiSlice";
+
+export const CategoryApiSlice = apiSlice
+  .enhanceEndpoints({
+    addTagTypes: ["Category"],
+  })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      createCategory: builder.mutation<
+        { category: Category },
+        { name: string; type: string }
+      >({
+        query: (args) => ({
+          url: "/categories",
+          method: "POST",
+          body: {
+            ...args,
+          },
+        }),
+        invalidatesTags: ["Category"],
+      }),
+      getCategories: builder.query<
+        {
+          income: Category[];
+          expense: Category[];
+        },
+        string
+      >({
+        query: (userId) => ({
+          url: `/users/${userId}/categories`,
+          method: "GET",
+        }),
+        transformResponse: (response: {
+          income: Category[];
+          expense: Category[];
+        }) => {
+          return {
+            income: response.income.sort((a, b) => a.order - b.order),
+            expense: response.expense.sort((a, b) => a.order - b.order),
+          };
+        },
+        providesTags: ["Category"],
+      }),
+    }),
+  });
+
+export const { useCreateCategoryMutation, useGetCategoriesQuery } =
+  CategoryApiSlice;
---
+++ apps/next-frontend/lib/features/Category/CategorySlice.tsx
@@ -0,0 +1,35 @@
+import { Category } from "@financemanager/financemanager-webiste-types";
+import { createSlice } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+
+import { RootState } from "@/lib/store";
+
+const CategorySlice = createSlice({
+  name: "category",
+  initialState: {
+    income: [] as Category[],
+    expense: [] as Category[],
+  },
+  reducers: {
+    setCategories(
+      state,
+      action: {
+        type: string;
+        payload: {
+          income: Category[];
+          expense: Category[];
+        };
+      },
+    ) {
+      state.income = action.payload["income"];
+      state.expense = action.payload["expense"];
+    },
+  },
+});
+
+export const { setCategories } = CategorySlice.actions;
+
+export default CategorySlice.reducer;
+
+export const useCategories = () =>
+  useSelector((state: RootState) => state.category);
