a5e2d982eee21bcf43708e349235147393469b3b
atree <anyu9898@yahoo.com>
2024-08-23 16:13:25 +0800

    feat(frontend): change the logic of duration component

--- apps/next-frontend/app/components/duration-filter/index.tsx
+++ apps/next-frontend/app/components/duration-filter/index.tsx
@@ -1,62 +1,90 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-interface StringState {
-  data: string;
-  setData: Dispatch<SetStateAction<string>>;
+interface DateState {
+  data: Date;
+  setData: Dispatch<SetStateAction<Date>>;
 }
 
-export default async function DurationFilter({
-  duration,
+export default function DurationFilter({
   startDate,
   endDate,
 }: {
-  duration: StringState;
-  startDate: StringState;
-  endDate: StringState;
+  startDate: DateState;
+  endDate: DateState;
 }) {
   const [show, setShow] = useState(false);
+  const [duration, setDuration] = useState("default");
+
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+
+  useEffect(() => {
+    switch (duration) {
+      case "week":
+        startDate.data.setDate(new Date().getDate() - 7);
+        break;
+      case "month":
+        startDate.data.setDate(new Date().getDate() - 30);
+        break;
+      case "3month":
+        startDate.data.setDate(new Date().getDate() - 90);
+        break;
+      case "ytd":
+        startDate.data.setDate(
+          new Date(new Date().getFullYear(), 0, 1).getDate(),
+        );
+        break;
+      case "customize":
+        startDate.setData(new Date(start));
+        endDate.setData(new Date(end));
+        break;
+      case "default":
+        break;
+    }
+  }, [duration, start, end, startDate, endDate]);
+
   return (
     <div className="flex flex-wrap justify-center z-10 text-black font-bold text-md">
       <div
-        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration.data === "default" ? "brightness-75" : ""}`}
+        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration === "default" ? "brightness-75" : ""}`}
         onClick={() => {
-          duration.setData("default");
+          setDuration("default");
           setShow(false);
         }}
       >
         不限
       </div>
       <div
-        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration.data === "7" ? "brightness-75" : ""}`}
+        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration === "week" ? "brightness-75" : ""}`}
         onClick={() => {
-          duration.setData("7");
+          setDuration("week");
           setShow(false);
         }}
       >
         近一周
       </div>
       <div
-        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration.data === "30" ? "brightness-75" : ""}`}
+        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration === "month" ? "brightness-75" : ""}`}
         onClick={() => {
-          duration.setData("30");
+          setDuration("month");
           setShow(false);
         }}
       >
         近一個月
       </div>
       <div
-        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration.data === "90" ? "brightness-75" : ""}`}
+        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration === "3month" ? "brightness-75" : ""}`}
         onClick={() => {
-          duration.setData("90");
+          setDuration("3months");
           setShow(false);
         }}
       >
         近三個月
       </div>
       <div
-        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration.data === "YTD" ? "brightness-75" : ""}`}
+        className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 cursor-pointer hover:brightness-75 ${duration === "ytd" ? "brightness-75" : ""}`}
         onClick={() => {
-          duration.setData("ytd");
+          setDuration("ytd");
           setShow(false);
         }}
       >
@@ -65,12 +93,12 @@ export default async function DurationFilter({
       <div
         className={`w-24 h-8 rounded-xl m-2 px-2 flex items-center justify-center border-[1px] border-slate-300 bg-primary-100 relative cursor-pointer`}
         onClick={() => {
-          duration.setData("customize");
+          setDuration("customize");
           setShow(!show);
         }}
       >
         自訂
-        {duration.data === "customize" && show && (
+        {duration === "customize" && show && (
           <div
             className="absolute top-10 bg-white p-4 rounded-lg shadow-lg w-64"
             onClick={(e) => e.stopPropagation()}
@@ -81,8 +109,8 @@ export default async function DurationFilter({
                 type="date"
                 id="start-date"
                 className="ml-2 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                value={startDate.data}
-                onChange={(e) => startDate.setData(e.target.value)}
+                value={start}
+                onChange={(e) => setStart(e.target.value)}
               />
             </div>
             <div className="flex justify-between items-center mb-4">
@@ -91,8 +119,8 @@ export default async function DurationFilter({
                 type="date"
                 id="end-date"
                 className="ml-2 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                value={endDate.data}
-                onChange={(e) => endDate.setData(e.target.value)}
+                value={end}
+                onChange={(e) => setEnd(e.target.value)}
               />
             </div>
             <button
