dd37330c4834382fd3bb1d2fceee678c777fab74
atree01 <anyu9898@yahoo.com>
2025-01-21 15:34:11 +0800

    chore: udpate some misc file

--- apps/nest-backend/data-source.ts
+++ apps/nest-backend/data-source.ts
@@ -1,13 +1,18 @@
+import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+console.log('db_name:', process.env.DB_NAME);
 
 const dataSource = new DataSource({
   type: 'mariadb',
   host: 'localhost',
   port: 3306,
-  username: 'root',
-  password: process.env.DB_ROOT_PASSWORD,
+  username: 'nest-backend',
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ['src/**/*.entity.js'],
+  entities: ['**/*.entity.js'],
   migrations: ['./src/migrations/*.ts'],
 });
 
